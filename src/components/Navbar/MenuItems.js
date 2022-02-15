55e38aa39bd7ce49d4aa181d464d608073fbce96
duyenlekha11 <duyenlekha1711@gmail.com>
2022-02-15 12:48:35 -0600

    Calorie Calculator

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Food Calories',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Health Tips',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
