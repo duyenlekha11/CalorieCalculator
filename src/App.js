e3aa8de856368f1b9b07851ee8b88792e3f83225
duyenlekha11 <duyenlekha1711@gmail.com>
2022-03-29 13:26:56 -0500

    Back to working after merge

--- src/App.js
+++ src/App.js
@@ -35,17 +35,7 @@ function App() {
     //   <Navbar />
       
     // </div>
-    <div className="App">
-      <Navbar />
-      <h1>How often do you excercise?</h1>
-      <Dropdown>
-        <Option selected value="Click to see options" />
-        <Option value="No excercise" />
-        <Option value="1-3 days a week" />
-        <Option value="4-5 days a week" />
-        <Option value="6-7 days a week" />
-      </Dropdown>
-    </div>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import react from 'react'
+import { Dropdown, Option } from "../components/Dropdown/Dropdown";
+
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="App">
+            <h1>How often do you excercise?</h1>
+            <Dropdown>
+                <Option selected value="Click to see options" />
+                <Option value="No excercise" />
+                <Option value="1-3 days a week" />
+                <Option value="4-5 days a week" />
+                <Option value="6-7 days a week" />
+            </Dropdown>
+        </div>
 
     )
 } 
