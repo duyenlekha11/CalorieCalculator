69d193485182bb63d3bf1e7105aff5e035a73a22
Christian Young <christian.michoal@gmail.com>
2022-04-27 11:01:59 -0500

    Added comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,78 +2,78 @@ import React, { Component } from 'react'
 import './home.css';
 class Home extends Component{
 
-    constructor(){
-        super();
-        this.state = {
-            gender: '',
-            weight:'',
-            age:'',
-            heightFeet:'',
-            heightInches:'',
-            activity:'',
-            bmr:'',
-            bmrsugg: '',
-            pal: '',
-            weightType: ''
+    constructor(){ //initialize objects state in a class
+        super(); //allows us to use this.props statements later on
+        this.state = { //assigning this.state
+            gender: '', //initializing state
+            weight:'', //initializing state
+            age:'', //initializing state
+            heightFeet:'', //initializing state
+            heightInches:'', //initializing state
+            activity:'', //initializing state
+            bmr:'', //initializing state
+            bmrsugg: '', //initializing state
+            pal: '', //initializing state
+            weightType: '' //initializing state
         }
     }
     handleAgeChange = (event) => {
-        this.setState({age: event.target.value})
+        this.setState({age: event.target.value}) //function to update states value 
     }
 
     handleWeightChange = (event) => {
-        this.setState({weight: event.target.value})
+        this.setState({weight: event.target.value}) //function to update states value 
     }
 
     handleheightFeetChange = (event) => {
-        this.setState({heightFeet: event.target.value})
+        this.setState({heightFeet: event.target.value}) //function to update states value 
     }
 
     handleheightInchesChange = (event) => {
-        this.setState({heightInches: event.target.value})
+        this.setState({heightInches: event.target.value}) //function to update states value 
     }
 
     handlegenderChange = (event) => {
-        this.setState({gender: event.target.value})
+        this.setState({gender: event.target.value}) //function to update states value 
     }
 
     handleactivityChange = (event) => {
-        this.setState({activity: event.target.value})
+        this.setState({activity: event.target.value}) //function to update states value 
     }
 
     handleweightTypeChange = (event) => {
-        this.setState({weightType: event.target.value})
+        this.setState({weightType: event.target.value}) //function to update states value 
     }
 
     calculateBMR(){
-        let age = this.state.age;
-        let gender = this.state.gender;
-        let heightFeet = this.state.heightFeet;
-        let heightInches = this.state.heightInches;
-        let weight = this.state.weight;
-        let weightType = this.state.weightType;
+        let age = this.state.age; //declaring shorter var names 
+        let gender = this.state.gender; //declaring shorter var names 
+        let heightFeet = this.state.heightFeet; //declaring shorter var names 
+        let heightInches = this.state.heightInches; //declaring shorter var names 
+        let weight = this.state.weight; //declaring shorter var names 
+        let weightType = this.state.weightType; //declaring shorter var names 
 
-        if(age == '' || gender == '' || heightFeet == '' || heightInches == '' || weight == ''){
+        if(age == '' || gender == '' || heightFeet == '' || heightInches == '' || weight == ''){ //if any fields are blank, display error
             this.setState({error:"All Fields are Required!"});
             return;
         }
         
-        let bmrCalc = '';
-        let height = ((heightFeet * 30.48) + (heightInches * 2.54));
-        if(weightType == 1){
-            if (gender == 2) {
-                bmrCalc = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age);
-            } else if (gender == 1) {
-                bmrCalc = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+        let bmrCalc = ''; //declare empty calculated bmr value
+        let height = ((heightFeet * 30.48) + (heightInches * 2.54)); //fllowing bmr formula
+        if(weightType == 1){ //weight type of imperial
+            if (gender == 2) { //male gender
+                bmrCalc = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age); //calculate based on gender and metric/imperial
+            } else if (gender == 1) { //female gender
+                bmrCalc = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age); //calculate based on gender and metric/imperial
             }
-        } else if(weightType == 2){
-            if (gender == 2) {
-                bmrCalc = 66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age);
-            } else if (gender == 1) {
-                bmrCalc = 655 + (9.563 * weight) + (1.85 * height) - (4.676 * age);
+        } else if(weightType == 2){ //weight type of metric
+            if (gender == 2) { //male gender
+                bmrCalc = 66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age); //calculate based on gender and metric/imperial
+            } else if (gender == 1) { //female gender
+                bmrCalc = 655 + (9.563 * weight) + (1.85 * height) - (4.676 * age); //calculate based on gender and metric/imperial
             }
         }
-        this.setState({bmr:bmrCalc})
+        this.setState({bmr:bmrCalc}) //updating that empty bmr to be value of bmrCalc
 
         // let activitySug = ';'
         // if (bmrCalc <= 1926) {
@@ -91,47 +91,47 @@ class Home extends Component{
         // }
         //this.setState({bmrsugg: "Suggestion: You should " +  activitySug + " in order to ?"})
 
-        this.setState({error:""});
-        console.log(this.state.weight)
+        //this.setState({error:""});
+        //console.log(this.state.weight)
     }
 
-    calculateKCalories() {
-        let resultPAL;
+    calculateKCalories() { //calculates kcal based on activity level
+        let resultPAL; //declare variable
         if (this.state.activity){
-            resultPAL = <div className="resultPAL">{this.state.bmr * this.state.activity}</div>
+            resultPAL = <div className="resultPAL">{this.state.bmr * this.state.activity}</div> //calculates kcal
         }
-        this.setState({pal:resultPAL})
+        this.setState({pal:resultPAL}) //unifies pal and its result variable
     }
 
  
-    render() {
+    render() { //renders the different values we display
         let error;
         if (this.state.error){
-            error = <div className="error">{this.state.error}</div>
+            error = <div className="error">{this.state.error}</div> //renders error messages
         }
 
         let resultBMR;
         if (this.state.bmr){
-            resultBMR = <div className="resultBMR">{this.state.bmr}</div>
+            resultBMR = <div className="resultBMR">{this.state.bmr}</div> //renders your bmr value
         }
 
         let resultSug;
         if (this.state.bmrsugg){
-            resultSug = <div className="resultSug">{this.state.bmrsugg}</div>
+            resultSug = <div className="resultSug">{this.state.bmrsugg}</div> //would render the suggestion value
         }
 
         let resultPAL;
         if (this.state.pal){
-            resultPAL = <div className="resultSug">{this.state.pal}</div>
+            resultPAL = <div className="resultSug">{this.state.pal}</div> //renders kcal
         }
 
        
 
-        return (
+        return ( //this builds the box and the various buttons/forms and the drop down that user interacts with
             <div id="bmrcalc">
             <div className="form">
-                <h2>BMR &amp; Daily Calorie Calculator</h2>
-                {error}
+                <h2>BMR &amp; Daily Calorie Calculator</h2> 
+                {error} 
                 <div className="inputwrap">
                     <label className="label">Gender</label><label>
                         <input type="radio" checked = {this.state.gender == "1"} onChange={this.handlegenderChange} className="genderM" name="gender" value="1" />Male</label><label>
